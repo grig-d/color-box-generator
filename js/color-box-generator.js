7cd48cb2654f242619e7c2822a27d1fcb453720f
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-02-06 01:26:15 +0200

    background color done

--- js/color-box-generator.js
+++ js/color-box-generator.js
@@ -1,23 +1,10 @@
-// Напиши скрипт создания и очистки коллекции элементов.
-// Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция.
-// При нажатии на кнопку Очистить, коллекция элементов очищается.
-
-// Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число.
-// Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
-
-// Каждый созданный div:
-
-// Имеет случайный rgb цвет фона
-// Размеры самого первого div - 30px на 30px
-// Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
-// Создай функцию destroyBoxes(), которая очищает div#boxes.
 
 const inputRef = document.querySelector('#controls input');
 const renderBtn = document.querySelector('button[data-action="render"]');
 const destroyBtn = document.querySelector('button[data-action="destroy"]');
 const boxesRef = document.querySelector('#boxes');
 
-let currentBoxSize = 30;
+let currentBoxSize = 200;
 
 // render
 renderBtn.addEventListener('click', renderHandler);
@@ -33,7 +20,7 @@ function createBoxes(amount) {
   for (let i = 0; i < amount; i += 1) {
     const boxRef = createBox(currentBoxSize);
     boxes.push(boxRef);
-    currentBoxSize += 10;
+    // currentBoxSize;
   }
   boxesRef.append(...boxes);
 }
@@ -59,7 +46,13 @@ function getRandomColor() {
 // destroy
 destroyBtn.addEventListener('click', destroyHandler);
 function destroyHandler() {
-  currentBoxSize = 30;
-  inputRef.value = '';
+  // currentBoxSize = 30;
+  // inputRef.value = '';
   boxesRef.querySelectorAll('div').forEach(element => element.remove());
 }
+
+// background color
+const backgroundBtn = document.querySelector('#background');
+backgroundBtn.addEventListener('click', event => {
+  boxesRef.classList.toggle('background');
+});
